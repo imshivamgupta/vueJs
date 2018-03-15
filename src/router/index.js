4fb2118eaa1bad9c0b795a5fc890c8cf6bd749b4
Ashu Tripathi <imsec.ashu@gmail.com>
2018-03-15 13:10:48 +0530

    fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Login from '@/components/Login'
-import Admin from '@/components/Admin'
-import Normal from '@/components/Normal'
+import HelloWorld from '../components/HelloWorld'
+import Login from '../components/Login'
+import Admin from '../components/Admin'
+import Normal from '../components/Normal'
 
 Vue.use(Router)
 
